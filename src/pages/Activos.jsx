151aa978b8c9fdc63f249c82025bc6a43098735c
AndresMoreta20 <andresmoreta2020@gmail.com>
2023-01-23 01:11:10 -0500

    nuevo

--- src/pages/Activos.jsx
+++ src/pages/Activos.jsx
@@ -12,7 +12,7 @@ const Activos = () => {
 
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-      <Header category="Page" title="Activos" />
+      <Header category="Page" title="Activos " />
       <button type='button'
               className='text-2x1 p-2
           hover:drop-shadow-xl
