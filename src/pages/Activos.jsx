24b3d3a224cd24dc897442ade8272055287dc72e
AndresMoreta20 <andresmoreta2020@gmail.com>
2023-01-24 16:20:05 -0500

    correcion errores 3

--- src/pages/Activos.jsx
+++ src/pages/Activos.jsx
@@ -45,7 +45,7 @@ const Activos = () => {
 
   useEffect(() => {
     cargarActivos();
-  }, [cargarActivos()])
+  });
   
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
