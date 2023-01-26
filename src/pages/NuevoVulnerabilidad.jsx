0188b4d858854f2b57b1c63b16e04d6d30a037f1
AndresMoreta20 <andresmoreta2020@gmail.com>
2023-01-25 19:20:18 -0500

    vilnerabilidades

--- src/pages/NuevoVulnerabilidad.jsx
+++ src/pages/NuevoVulnerabilidad.jsx
@@ -48,7 +48,7 @@ const NuevoVulnerabilidad = () => {
       "amenazas": amenazaS,
       "calificacion": calificacionS,
       "especificacion": especificacionS,
-      "severidad": severidad,
+      "severidad": severidadS,
     });
     console.log(res);
     navigate('/Vulnerabilidades')
